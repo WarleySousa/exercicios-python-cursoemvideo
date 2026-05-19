bb05f988189feb765fec9a81636bffeda3c4331b
Warley <warley.sousa8@gmail.com>
2026-05-19 18:36:44 -0300

    Unidade de medida

---
+++ ex009 .py
@@ -0,0 +1,4 @@
+medida1 = float(input('Digite a metragem: '))
+centimetros = medida1*100
+print('A metragem digitada foi {}m, e sua conversão para centímetros é {}cm!' .format(medida1, centimetros))
+
