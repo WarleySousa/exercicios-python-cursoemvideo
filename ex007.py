4fe9be0da0206da93d4bb3b1ba43d102219ff7ac
Warley <warley.sousa8@gmail.com>
2026-05-19 18:24:29 -0300

    mais um exercicio

---
+++ ex007.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um numeral: '))
+d = n1*2
+t = n1*3
+rq = n1**(1/2) 
+print('O numero digitado foi {}, seu dobro é {}, seu triplo é {}, e sua raiz quadrada é {:.2f}!' .format(n1, d, t, rq))
