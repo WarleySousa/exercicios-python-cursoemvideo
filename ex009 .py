f8ac8ba2f9378ed17b707479ae51b642cbdbd6d6
Warley <warley.sousa8@gmail.com>
2026-06-01 20:35:08 -0300

    Alterações de melhorias e complementos.

--- ex009 .py
+++ ex009 .py
@@ -1,4 +1,6 @@
 medida1 = float(input('Digite a metragem: '))
 centimetros = medida1*100
-print('A metragem digitada foi {}m, e sua conversão para centímetros é {}cm!' .format(medida1, centimetros))
+milimetros = medida1*1000
+print('A metragem digitada foi {}m,  \nSua conversão para centímetros é {}cm, \nSua conversão em milimitros é {}mm!' .format(medida1, centimetros, milimetros))
+
 
--- ex011.py
+++ ex011.py
@@ -1,3 +1,5 @@
-real = float(input('Digite o valor em reais: '))
-dolar = real/5.25
-print('O valor digitado foi R${:.2f}, e sua conversão para dólares é US${:.2f}!' .format(real, dolar))
+real = float(input('Digite o valor em reais: R$'))
+dolar = real/5.04
+euro = real/5.86
+cad = real/3.64
+print('O valor digitado foi R${:.3f} \nSua conversão para dólares é US${:.3f} \nEm euro será de €{:.3f} \nEm Dólar canadense você consiguirá comprar C${:.3f}! \n\n A vida não é fácil, bora trabalhar! ' .format(real, dolar, euro, cad))
