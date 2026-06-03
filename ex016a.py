a5d81868c9222effd70368b0ee5b70a6409e98c7
Warley <warley.sousa8@gmail.com>
2026-06-03 19:08:17 -0300

    Dois tipos de variações com números inteiros de matemática. O primeiro está como comentário e foi feito da forma na qual se importa o comando trunc da biblioteca math. O segundo foi feito de forma sem importação, usando apenas o int no print.

---
+++ ex016a.py
@@ -0,0 +1,6 @@
+'''from math import trunc
+numero = float(input('Digite um numero: '))
+print('O numero digitado foi {} e a sua porção inteira é {}'.format(numero, trunc(numero)))'''
+
+numero = float(input('Digite um numero: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(numero, int(numero)))
