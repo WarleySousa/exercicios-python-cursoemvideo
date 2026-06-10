2120a74083084df4f75a97321d2f3e31e701bca1
Warley <warley.sousa8@gmail.com>
2026-06-10 19:30:30 -0300

    Criando um analisador de textos

--- ex022.py
+++ ex022.py
@@ -4,3 +4,5 @@ print('Seu nome em maíusculas é {}'.format(nome.upper()))
 print('Seu nome em minúscula é {}'.format(nome.lower()))
 print('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
 print('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
+
+
